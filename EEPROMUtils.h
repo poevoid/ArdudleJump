4ad36273f385dacafc222a0d4f3fc56b9e56474e
poevoid <65972401+poevoid@users.noreply.github.com>
2020-10-24 11:24:16 -0500

    Update EEPROMUtils.h

--- EEPROMUtils.h
+++ EEPROMUtils.h
@@ -9,10 +9,10 @@ void initEEPROM() {
   uint8_t c1 = EEPROM.read(EEPROM_START_C1);
   uint8_t c2 = EEPROM.read(EEPROM_START_C2);
 
-  if (c1 != 'S' || c2 != 'T') {   
+  if (c1 != 'A' || c2 != 'J') {   
       
-    EEPROM.update(EEPROM_START_C1, 'S');
-    EEPROM.update(EEPROM_START_C2, 'T');
+    EEPROM.update(EEPROM_START_C1, 'A');
+    EEPROM.update(EEPROM_START_C2, 'J');
     EEPROM.put(EEPROM_SCORE, (uint16_t)0);
           
   }
